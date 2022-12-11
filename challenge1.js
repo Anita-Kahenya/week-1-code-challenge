38d625636f8e830bd19f76db07a3c9d13a636360
ray-nay <116163593+ray-nay@users.noreply.github.com>
2022-12-11 17:09:10 +0300

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -1,26 +1,24 @@
-// function to output the expected grade based on marks 
+let marks=document.getElementById("grade")
+let grade=document.getElementById("show-grade")
+let result=document.getElementById("your-grade")
 function gradeGenerator (marks){
 
     if (marks >79) {
-        console.log ("A")
+        result.textContent="A"
     } else if (marks <=79 && marks >60){
-        console.log ("B")
+        result.textContent="B"
     } else if (marks <=59 && marks >49){
-        console.log ("C")
+        result.textContent="C"
     }else if (marks <=49 && marks>40){
-        console.log ("D")
+        result.textContent="D"
     } else {
-        console.log ("E")
+        result.textContent="E"
     }
 }
 
-//user prompt to enter marks 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  readline.question('Enter marks ',marks => {
-    gradeGenerator(marks)
-    readline.close();
-  })
+
+
+grade.addEventListener("click", (event)=>{
+    gradeGenerator(marks.value)
+   // console.log(marks.value)
+})
