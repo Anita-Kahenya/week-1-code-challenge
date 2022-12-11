18ef4d88564261038d73f7f5cc01c9301ebe7ea6
ray-nay <116163593+ray-nay@users.noreply.github.com>
2022-12-11 17:09:54 +0300

    Update challenge2.js

--- challenge2.js
+++ challenge2.js
@@ -1,26 +1,24 @@
+let speed=document.getElementById("speed")
+let calculatedPoints=document.getElementById("calc-demerit")
+let demeritPoints=document.getElementById("demerit-points")
+
 // function to calculate the demerit points and the expected output
 function speedDetector(speed){
     if (speed<70) {
-        console.log ("Ok")
-        
+        demeritPoints.textContent = "Ok"        
     }else {
         const point=Math.floor((speed-70)/5) 
         if (point<12) {
-            console.log ("points: " + point)
+            demeritPoints.textContent= `points:   ${point}`
         }
         else {
-            console.log ("License suspended")
+            demeritPoints.textContent= "License suspended"
         }
     }
 }
 
-//user prompt to enter speed 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  readline.question('Enter speed ',speed => {
-    speedDetector(speed)
-    readline.close();
-  })
+
+
+calculatedPoints.addEventListener("click", (event)=>{
+    speedDetector(speed.value)
+})
